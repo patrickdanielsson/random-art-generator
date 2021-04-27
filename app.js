2571f90f8d0e0eff97ab853a19289363961e0fd2
jtduchesne <jtduchesne@gmail.com>
2021-04-26 20:53:10 -0400

    Add random corner radius

--- app.js
+++ app.js
@@ -9,20 +9,26 @@ document.onreadystatechange = function() {
             let rectHeight = randomPixel(height/10, height/(random(1, iteration) + 1));
             let rectWidth  = randomPixel(width/10, width/(random(1, iteration) + 1));
 
+            let rectRadius     = randomBoolean(2);
+            let rectRadiusBoth = randomBoolean(2);
+
             let tilt = randomDegree(0, 180, 22.5);
             let opacity = Math.random();
             let color = `${random(50, 255, 5)},${random(50, 255, 5)},${random(50, 255, 5)}`;
 
-            let rectBackground = `linear-gradient(${tilt}, rgba(${color}, ${opacity}), rgba(${color}, 0))`;
+            let rectBackground = `linear-gradient(${tilt}, rgba(${color}, ${opacity}), rgba(${color}, 0))`
 
-            let inc = random(10, 180, 12);
+            let inc = random(10, 190, 12);
             let rotation = inc;
             for (let security=360; security > 0; security--) {
                 let rect = document.createElement('div');
                 rect.style.position = "absolute";
                 rect.style.height = rectHeight;
                 rect.style.width  = rectWidth;
-                rect.style.borderTopRightRadius = rectHeight;
+                if (rectRadius) {
+                    rect.style.borderTopRightRadius = rectHeight;
+                    if (rectRadiusBoth) rect.style.borderBottomLeftRadius = rectHeight;
+                }
                 rect.style.background = rectBackground;
 
                 rect.style.transformOrigin = "top left";
@@ -39,5 +45,6 @@ document.onreadystatechange = function() {
         function random(min, max, res=1)        { return Math.floor((Math.random() * (max - min) + min) / res) * res; }
         function randomDegree(min, max, res=10) { return random(min, max, res) + "deg"; }
         function randomPixel(min, max, res=10)  { return random(min, max, res) + "px"; }
+        function randomBoolean(bias=1)          { return !!Math.round(Math.random() * bias); }
     }
 };
